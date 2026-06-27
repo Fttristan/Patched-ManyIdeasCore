d7c863c4f60d001239bc4494d6a5e49f87c52b80
FTTristan <135572396+Fttristan@users.noreply.github.com>
2026-06-27 23:29:23 +0000

    Fix Private Stub?

--- src/main/java/de/geheimagentnr1/minecraft_forge_api/AbstractMod.java
+++ src/main/java/de/geheimagentnr1/minecraft_forge_api/AbstractMod.java
@@ -1,11 +1,10 @@
 package de.geheimagentnr1.minecraft_forge_api;
 import net.minecraftforge.eventbus.api.IEventBus;
 import java.util.function.Function;
-import de.geheimagentnr1.manyideas_core.ManyIdeasCore;
 
 public abstract class AbstractMod {
-    public <T> T registerEventHandler(T handler) { return handler; }
-    public <T> T registerConfig(Function<ManyIdeasCore, T> configFactory) { return null; }
+    public Object registerEventHandler(Object handler) { return null; }
+    public Object registerConfig(Function<?, ?> configFactory) { return null; }
     public IEventBus forgeEventBus() { return null; }
     public IEventBus modEventBus() { return null; }
     protected abstract void initMod();

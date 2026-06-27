5fcdd6284ac0bb9d6807f1184a7edf1034e15a56
FTTristan <135572396+Fttristan@users.noreply.github.com>
2026-06-27 23:20:04 +0000

    aaa

---
+++ src/main/java/de/geheimagentnr1/manyideas_core/patch/ManyIdeasCorePatch.java
@@ -0,0 +1,12 @@
+package de.geheimagentnr1.manyideas_core.patch;
+
+import net.minecraftforge.fml.common.Mod;
+
+// This Mod ID MUST match the one in your mods.toml
+@Mod("manyideas_core_patch")
+public class ManyIdeasCorePatch {
+    public ManyIdeasCorePatch() {
+        // This class exists only to satisfy Forge's mod loader.
+        // All the actual work is done by your Mixin.
+    }
+}

282e3774b208c6edc989fbff4b4975d809a9109b
FTTristan <135572396+Fttristan@users.noreply.github.com>
2026-06-27 23:28:08 +0000

    a

--- src/main/java/de/geheimagentnr1/manyideas_core/mixin/ManyIdeasCoreMixin.java
+++ src/main/java/de/geheimagentnr1/manyideas_core/mixin/ManyIdeasCoreMixin.java
@@ -13,58 +13,84 @@ import de.geheimagentnr1.manyideas_core.elements.recipes.ModRecipeSerializersReg
 import de.geheimagentnr1.manyideas_core.elements.recipes.ModRecipeTypesRegisterFactory;
 import de.geheimagentnr1.manyideas_core.network.Network;
 import de.geheimagentnr1.manyideas_core.special.decoration_renderer.PlayerDecorationManager;
-import de.geheimagentnr1.minecraft_forge_api.AbstractMod;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.function.Function;
 
 @Mixin(value = ManyIdeasCore.class, remap = false)
-public abstract class ManyIdeasCoreMixin extends AbstractMod {
+public abstract class ManyIdeasCoreMixin {
+
+    // These shadows use 'Object' to match the compiled bytecode exactly, 
+    // avoiding the NoSuchMethodError (Signature mismatch).
+    @Shadow(remap = false) 
+    public abstract Object registerEventHandler(Object handler);
+
+    @Shadow(remap = false) 
+    public abstract Object registerConfig(Function<?, ?> configFactory);
+
+    @Shadow(remap = false) 
+    public abstract IEventBus forgeEventBus();
+
+    @Shadow(remap = false) 
+    public abstract IEventBus modEventBus();
 
     /**
      * @author GeheimagentNr1 (Fixed by Patch)
-     * @reason Fixes Dedicated Server crash by wrapping Client-only logic in DistExecutor
+     * @reason Fix Dedicated Server crash by isolating client-only factory registrations.
      */
     @Overwrite(remap = false)
-    @Override
     protected void initMod() {
-        ModBlocksRegisterFactory modBlocksRegisterFactory = registerEventHandler(new ModBlocksRegisterFactory());
+        // 1. Common Side Registrations (Safe for Server)
+        ModBlocksRegisterFactory blocksFactory = (ModBlocksRegisterFactory) registerEventHandler(new ModBlocksRegisterFactory());
         registerEventHandler(new ModArgumentTypesRegisterFactory());
         registerEventHandler(new ModCommandsRegisterFactory());
-        ModItemsRegisterFactory modItemsRegisterFactory = registerEventHandler(new ModItemsRegisterFactory());
+        
+        ModItemsRegisterFactory itemsFactory = (ModItemsRegisterFactory) registerEventHandler(new ModItemsRegisterFactory());
 
+        // Cast this to ManyIdeasCore for the ingredient factory
         ManyIdeasCore instance = (ManyIdeasCore) (Object) this;
-
         registerEventHandler(new ModIngredientSerializersRegisterFactory(instance));
+        
         registerEventHandler(new ModRecipeSerializersRegisterFactory());
         registerEventHandler(new ModRecipeTypesRegisterFactory());
         registerEventHandler(Network.getInstance());
 
-        // Note: Added 'return null;' inside the suppliers to fix the 'missing return value' error
+        // 2. Client-Only Registrations (Isolated from Server)
         DistExecutor.unsafeRunForDist(
             () -> () -> {
-                ClientConfig clientConfig = registerConfig(ClientConfig::new);
+                // This code ONLY runs on the Physical Client.
+                // We use registerConfig and registerEventHandler for client classes here.
+                
+                // Load Client Config
+                ClientConfig clientConfig = (ClientConfig) registerConfig((Function<ManyIdeasCore, ClientConfig>) ClientConfig::new);
 
-                ModDebugBlocksRegisterFactory modDebugBlocksRegisterFactory = registerEventHandler(
+                // Load Debug Blocks (Client Only)
+                ModDebugBlocksRegisterFactory debugBlocksFactory = (ModDebugBlocksRegisterFactory) registerEventHandler(
                     new ModDebugBlocksRegisterFactory(clientConfig)
                 );
 
+                // Load Creative Tabs (Client Only)
                 registerEventHandler(new ModCreativeModeTabRegisterFactory(
                     clientConfig,
-                    modBlocksRegisterFactory,
-                    modDebugBlocksRegisterFactory,
-                    modItemsRegisterFactory
+                    blocksFactory,
+                    debugBlocksFactory,
+                    itemsFactory
                 ));
 
+                // Handle Player Decorations (Client Only)
                 PlayerDecorationManager playerDecorationManager = new PlayerDecorationManager();
                 forgeEventBus().addListener(playerDecorationManager::handlePreRenderPlayerEvent);
                 modEventBus().addListener(playerDecorationManager::handleFMLClientSetupEvent);
                 
-                return null; // Fixed: Innermost supplier must return a value (T)
+                return null;
             },
             () -> () -> {
-                // Server side - do nothing
-                return null; // Fixed: Innermost supplier must return a value (T)
+                // Server Side: Skip all client logic
+                return null;
             }
         );
     }
